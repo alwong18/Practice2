ce27f2bcb724122ff4ac694adb920fa654482e2f
alwong18 <alwong18@s.sfusd.edu>
2023-12-18 17:43:33 -0800

    Sound Effects Added

--- Player.gd
+++ Player.gd
@@ -2,6 +2,9 @@ extends Area2D
 
 @export var speed = 400
 
+@onready var bg_music = $"../BGMusic"
+@onready var enemy_hit = $"../EnemyHit"
+
 func _process(delta):
 	var velocity = Vector2.ZERO
 	
@@ -22,3 +25,5 @@ func _process(delta):
 
 func _on_body_entered(body):
 	hide()
+	bg_music.stop()
+	enemy_hit.play()
